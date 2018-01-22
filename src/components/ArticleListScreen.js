1aeff6fee24e1ca85b3c2098b96e7bb709bf49e9
bong kim <32095134+underbleu@users.noreply.github.com>
2018-01-22 12:55:21 +0900

    bulletin list output

--- src/components/ArticleListScreen.js
+++ src/components/ArticleListScreen.js
@@ -1,14 +1,53 @@
 import React, {Component} from 'react';
+import {Table} from 'semantic-ui-react';
+import styled from 'styled-components';
 import NavBar from './NavBar';
+
+const mockData = [
+  {
+    articleId: "-LB1",
+    title: "제목1",
+    author: "김봉",
+    createdAt: "2018-01-22"
+  },
+  {
+    articleId: "-LB2",
+    title: "제목2",
+    author: "족요",
+    createdAt: "2018-01-22"
+  }
+];
+
+const ArticleItemRow = styled(Table.Row)`
+  &:hover{
+    cursor: pointer;
+    background-color: beige;
+  }
+`
 
 export default class ArticleListScreen extends Component {
   render() {
     const {nickName, onNickNameClick} = this.props;
-    return (
-      <div>
+    return <div>
         <NavBar nickName={nickName} onNickNameClick={onNickNameClick} />
-        게시글 목록
-      </div>
-    )
+        <Table>
+          <Table.Header>
+            <ArticleItemRow>
+              <Table.HeaderCell>작성자</Table.HeaderCell>
+              <Table.HeaderCell>제목</Table.HeaderCell>
+              <Table.HeaderCell>작성일</Table.HeaderCell>
+            </ArticleItemRow>
+          </Table.Header>
+          <Table.Body>
+            {mockData.map(({ articleId, title, author, createdAt }) => (
+              <ArticleItemRow key={articleId}>
+                <Table.Cell>{title}</Table.Cell>
+                <Table.Cell>{author}</Table.Cell>
+                <Table.Cell>{createdAt}</Table.Cell>
+              </ArticleItemRow>
+            ))}
+          </Table.Body>
+        </Table>
+      </div>;
   }
 }
--- src/components/BBS.js
+++ src/components/BBS.js
@@ -16,19 +16,22 @@ export default class BBS extends Component {
   }
   componentDidMount() {
     const config = {
-      apiKey: "AIzaSyC5bvpoKyfa3qwTxhSt0PxgQZI2dI3QbZc",
-      authDomain: "fds-cra.firebaseapp.com",
-      databaseURL: "https://fds-cra.firebaseio.com",
-      projectId: "fds-cra",
-      storageBucket: "fds-cra.appspot.com",
-      messagingSenderId: "966283711333"
+      apiKey: "AIzaSyBU_1iDe46dKqNJBCjNHjB2u9zY_-HBhvA",
+      authDomain: "fds-cra-c0094.firebaseapp.com",
+      databaseURL: "https://fds-cra-c0094.firebaseio.com",
+      projectId: "fds-cra-c0094",
+      storageBucket: "fds-cra-c0094.appspot.com",
+      messagingSenderId: "497087072093"
     };
     firebase.initializeApp(config);
-    firebase.auth().onAuthStateChanged(user => {
+    firebase.auth().onAuthStateChanged(async user => {
       if (user) {
+        const snapshot = await firebase.database().ref(`users/${user.uid}/nickName`).once('value');
+        console.log(snapshot.val());
         this.setState({
           page: 'list',
-          uid: user.uid
+          uid: user.uid,
+          nickName: snapshot.val()
         });
       } else {
         this.setState({
@@ -39,27 +42,31 @@ export default class BBS extends Component {
   }
   saveNickName = async nickName => {
     const {uid} = this.state;
-    await firebase.database().ref(`users/${uid}/nickName`).set(nickName)
+    await firebase.database().ref(`users/${uid}/nickName`).set(nickName);
     this.setState({
       nickName,
       page: 'list'
     });
   }
   render() {
+    const {nickName, uid} = this.state;
     return (
       <div>
         {
           this.state.page === 'login'
           ? <LoginScreen />
           : this.state.page === 'list'
-          ? <ArticleListScreen onNickNameClick={this.pageToAccount} nickName={this.state.nickName ? this.state.nickName : this.state.uid} />
+          ? <ArticleListScreen 
+              onNickNameClick={this.pageToAccount} 
+              nickName={nickName || uid} />
           : this.state.page === 'account'
           ? <AccountScreen 
               onNickNameClick={this.pageToAccount} 
-              nickName={this.state.nickName ? this.state.nickName : this.state.uid}
+              nickName={nickName || uid}
               onNickNameSubmit={this.saveNickName} />
           : null
         }
+
       </div>
     )
   }
